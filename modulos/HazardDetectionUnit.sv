52432efa3c18596d8b285215fdf9f33ccd52b43a
stivcs <stiven.castro@utp.edu.co>
2024-06-03 19:58:44 -0500

    eliminacion del clk

--- modulos/HazardDetectionUnit.sv
+++ modulos/HazardDetectionUnit.sv
@@ -3,13 +3,12 @@ module HazardDetectionUnit(
 	input logic [4:0] rs2_de,
 	input logic [4:0] rd_ex,
 	input logic DMRd_ex, //para saber si va a leer en la data memori, 0 si lee en memoria
-	input logic clk,
 	output logic clr,
 	output logic en_pc_inc_de, // eneblo de pc_inc
 	output logic en_pc_fe // eneblo de pc_fe
 );
 
-	always_ff @(posedge clk)
+	always @* begin
 		if (!DMRd_ex && ((rs1_de == rd_ex) || (rs2_de == rd_ex)))
 			begin
 				clr = 1'b1;
@@ -22,5 +21,5 @@ module HazardDetectionUnit(
 				en_pc_inc_de = 1'b1;
 				en_pc_fe = 1'b1;
 			end
-	
+	end
 endmodule
